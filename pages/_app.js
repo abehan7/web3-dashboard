371d6da72d1ff3b039397efb871e7218e8a85d82
abehan7 <hanjk13262@gmail.com>
2022-07-05 09:16:30 +0900

    add:> web3

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
+import { MoralisProvider } from "react-moralis";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import "../styles/AutoHeightImage.css";
 
-import { MoralisProvider } from "react-moralis";
-
 function MyApp({ Component, pageProps }) {
+  // console.log(ChainId);
   return (
-    <MoralisProvider
-      serverUrl={process.env.NEXT_PUBLIC_DAPP_URL}
-      appId={process.env.NEXT_PUBLIC_APP_ID}
-    >
-      <Component {...pageProps} />
-    </MoralisProvider>
+    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+      <MoralisProvider
+        serverUrl={process.env.NEXT_PUBLIC_DAPP_URL}
+        appId={process.env.NEXT_PUBLIC_APP_ID}
+      >
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </ThirdwebProvider>
   );
 }
 
